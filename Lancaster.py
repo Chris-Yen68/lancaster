e8ff0717dcd56d07d7756e008f7418c17ea15e69
LiZhen <thereaghostflash@gmail.com>
2020-11-24 17:06:32 +0800

    updating and fixed the issue

--- Lancaster.py
+++ Lancaster.py
@@ -37,6 +37,8 @@ p_list = [soup.title.string] + soup.find_all('p')
 tokenizer = nltk.RegexpTokenizer(r'\w+')
 st = []
 title = set()
+POS = set()
+
 for i,p in enumerate(p_list):
     if p.string != None:
         word_list = tokenizer.tokenize(p.string)
@@ -48,24 +50,32 @@ for i,p in enumerate(p_list):
             for ele in chunk:
                 if not isinstance(ele,nltk.Tree) and ele[1] == 'NNS':
                     title.add(ele[0])
+                elif not isinstance(ele,nltk.Tree):
+                    POS.add(ele)
             if NE:
                 title |= set(NE)
         else:
-#             calculate the number of matching tagged words            
+#             calculate the number of matching NE and POS tagged words            
             m = 0
             for c in NE:
                 if c in title:
                     m += 1   
             for ele in chunk:
-                if not isinstance(ele,nltk.Tree) and ele[0] in title:
+                if not isinstance(ele,nltk.Tree) and (ele[0] in title or ele in POS):
                     m += 1
-                    
+#              sorting based on matching number increasingly and index from article decreasingly, so that we can add top ranking sentence with same mathing number in original order       
             if m:
-                heapq.heappush(st,(m,p.string))
-            if len(st) > 12:
-                heapq.heappop(st)
-        
-print("".join(s for _,s in st[::-1]))
+                heapq.heappush(st,[m,-i,p.string])
+
+res = []
+total_words = 0
+for _,_,s in st[::-1]:
+    total_words += len(s.string)
+    res.append(s.string)
+    if total_words > 140:
+        break
+
+print(','.join(res))
 
 
 
